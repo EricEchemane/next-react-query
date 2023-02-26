135878c7b390ef6a57d7ba744d68a0ec80ca50ec
Eric Echemane <eechemane29@gmail.com>
2023-02-26 18:36:49 +0800

    api/todos created passing

---
+++ __tests__/api/todos.spec.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import todosApiHandler from '@/pages/api/todos';
+import { Mocks, createMocks } from 'node-mocks-http';
+
+describe('/api/todos', () => {
+	let httpMock: Mocks<NextApiRequest, NextApiResponse>;
+
+	describe('GET', () => {
+		beforeEach(() => {
+			httpMock = createMocks({
+				method: 'GET',
+			});
+		});
+
+		it('should return 200 response', async () => {
+			const { req, res } = httpMock;
+			await todosApiHandler(req, res);
+			expect(res._getStatusCode()).toBe(200);
+		});
+
+		it('should return an empty array', async () => {
+			const { req, res } = httpMock;
+			await todosApiHandler(req, res);
+			const data = JSON.parse(res._getData());
+			expect(data).toEqual([]);
+		});
+	});
+});
---
+++ src/pages/api/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from '@/interfaces/todo.int';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const todos: Todo[] = [];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	const { method } = req;
+	switch (method) {
+		case 'GET':
+			res.status(200).json(todos);
+			break;
+		case 'POST':
+			const { title } = req.body;
+			const id = todos.length + 1;
+			const todo = { id, title };
+			todos.push(todo);
+			res.status(201).end();
+			break;
+	}
+}
