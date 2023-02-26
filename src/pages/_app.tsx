ab18923269abbc484ce1b094899e70f05e4960d2
Eric Echemane <eechemane29@gmail.com>
2023-02-26 18:02:21 +0800

    Revert "implements reacy-query"
    This reverts commit b74868a4bc326b57edd1d3a4d2f4f56eeba3d43f.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,6 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { QueryClientProvider, QueryClient } from 'react-query';
-
-const queryClient = new QueryClient();
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<Component {...pageProps} />
-			</QueryClientProvider>
-		</>
-	);
+  return <Component {...pageProps} />
 }
